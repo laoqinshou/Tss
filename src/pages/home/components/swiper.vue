90ce761c433c21b63ccc2a592ef804b823e7b083
laoqinshou <82282095@qq.com>
2019-10-04 18:24:13 +0800

    10/4

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="items of swiperList" :key="items.id">
+        <img
+          class="swiper-img"
+          :src="items.imgurl"
+        />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '1111',
+        imgurl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3081108087,555312235&fm=26&gp=0.jpg'
+      },
+      {
+        id: '222',
+        imgurl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1392403833,353777295&fm=26&gp=0.jpg'
+      },
+      {
+        id: '333',
+        imgurl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2939168414,2525881912&fm=26&gp=0.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style scoped lang="stylus">
+.wapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+
+  .swiper-img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div>sdff</div>
   </div>
 </template>
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
-<style scoped>
+<style scoped lang="stylus">
 
 </style>
