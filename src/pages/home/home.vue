cb6b303688e4fdf2f98c2f46869d85cea10a9fe1
laoqinshou <82282095@qq.com>
2019-10-23 17:40:25 +0800

    comm

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,18 +3,21 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-comm></home-comm>
   </div>
 </template>
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/icons'
+import HomeComm from './components/comm'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeComm
   }
 }
 </script>
